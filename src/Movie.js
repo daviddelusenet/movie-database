21c62d511fdc94c053e8ba2288d50dc20e559481
David de Lusenet <lusenet@momkai.com>
2017-11-23 15:57:18 +0100

    Added prop-types to Movie component

--- src/Movie.js
+++ src/Movie.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class Movie extends Component {
+  static propTypes = {
+    movie: PropTypes.shape({
+      desc: PropTypes.string.isRequired,
+      title: PropTypes.string
+    })
+  };
+
+  static defaultProps = {
+    desc: 'Description not available'
+  };
+
   render() {
     return(
-      <div>{this.props.movie.title}</div>
+      <div>
+        <h3 className="Movie__title">{this.props.movie.title}</h3>
+        <p className="Movie__desc">{this.props.movie.desc}</p>
+      </div>
     );
   }
 };
