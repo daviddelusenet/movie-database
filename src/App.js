f8b50fafd992b5e7dc82c7bcc4c71bef5a836b6d
David de Lusenet <lusenet@momkai.com>
2017-11-23 15:10:15 +0100

    First setup of Movie component

--- src/App.js
+++ src/App.js
@@ -2,17 +2,24 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Import child components
+import Movie from './Movie';
+
+const Movies = [
+  {
+    "id": 1,
+    "title": "Best movie ever"
+  }
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Movie Database</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        {Movies.map((movie) => <Movie key={movie.id} movie={movie} />)}
       </div>
     );
   }
