128aad5b3e5785153b909c7178adbd5c25fefb13
David de Lusenet <lusenet@momkai.com>
2017-11-24 14:45:31 +0100

    Refactored App.js to styled-components + added reset.css

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import {
   Switch,
   Link
 } from 'react-router-dom';
-import './App.css';
+import styled from 'styled-components';
 
 // Import child components
 import MovieDetail from './MovieDetail';
@@ -14,16 +14,32 @@ import MovieList from './MovieList';
 
 const App = () => (
   <Router>
-    <div className="App">
-      <header className="App__header">
-        <Link to="/" className="App__title">Movie Database</Link>
-      </header>
+    <MovieDatabase>
+      <Header>
+        <Title to="/">Movie Database</Title>
+      </Header>
       <Switch>
         <Route exact path="/" component={MovieList} />
         <Route path="/movies/:id" component={MovieDetail} />
       </Switch>
-    </div>
+    </MovieDatabase>
   </Router>
 );
 
 export default App;
+
+const MovieDatabase = styled.div`
+  text-align: center;
+`;
+
+const Header = styled.header`
+  background-color: #111;
+  padding: 40px 20px;
+`;
+
+const Title = styled(Link)`
+  color: white;
+  text-decoration: none;
+  margin: 0;
+  font-size: 24px;
+`;
